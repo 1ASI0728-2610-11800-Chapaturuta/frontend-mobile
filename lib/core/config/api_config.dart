c83301204fd9e5c585001dcb77b8b4fe8eb46d44
Néstor Velarde Gonzales <u20211c221@upc.edu.pe>
2026-06-13 15:37:48 -0500

    chore: update API base URL and add missing imports in theme

--- lib/core/config/api_config.dart
+++ lib/core/config/api_config.dart
@@ -1,6 +1,6 @@
 class ApiConfig {
   static const String baseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'http://10.0.2.2:5027/api',
+    defaultValue: 'https://frock-backend.purplestone-add9d7b8.eastus.azurecontainerapps.io/api',
   );
 }
